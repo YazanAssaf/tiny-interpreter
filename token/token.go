3c2a59ae2a87563c73276e9322809a924a884fb6
YazanAssaf <yazan.assaf.m@gmail.com>
2025-06-29 12:19:04 +0300

    add token type constants

--- token/token.go
+++ token/token.go
@@ -1,1 +1,34 @@
 package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	// Special tokens
+	ILLEGAL TokenType = "ILLEGAL" // Represents an unknown or illegal token
+	EOF     TokenType = "EOF"     // Represents the end of the input file
+
+	// Identifiers + literals
+	IDENT TokenType = "IDENT"
+	INT   TokenType = "INT"
+
+	// Operators
+	ASSIGN TokenType = "="
+	PLUS   TokenType = "+"
+
+	// Delimiters
+	COMMA     TokenType = ","
+	SEMICOLON TokenType = ";"
+	LPAREN    TokenType = "("
+	RPAREN    TokenType = ")"
+	LBRACE    TokenType = "{"
+	RBRACE    TokenType = "}"
+
+	// Keywords
+	FUNCTION TokenType = "FUNCTION"
+	LET      TokenType = "LET"
+)
